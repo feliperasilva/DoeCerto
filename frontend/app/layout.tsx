473c93a50cc2bf529211f13a8e24ebabaaa33e4b
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-07 00:42:56 -0300

    feat: update layout to include header and footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
+import { roboto, darkerGrotesque } from './fonts'
 import type { Metadata } from 'next'
 import { ReactNode } from 'react'
+import { Header, Footer } from '@/components'
 
 export const metadata: Metadata = {
     title: 'DoeCerto',
@@ -9,8 +11,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="pt-BR">
-            <body>{children}</body>
+        <html lang="pt-BR" className={`${roboto.variable} ${darkerGrotesque.variable}`}>
+            <body>
+                <Header />
+                <main>{children}</main>
+                <Footer/>
+            </body>
         </html>
     )
 }
