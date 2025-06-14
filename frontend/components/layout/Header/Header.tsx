5157be0d0f5dee07bb032d65d9f46bad5964dc83
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-14 11:39:40 -0300

    refactor: moving Header.tsx types to @/types

--- frontend/components/layout/Header/Header.tsx
+++ frontend/components/layout/Header/Header.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import styles from "./Header.module.css";
-import { Logo } from "@/components"
+import { Logo } from "@/components";
+import Link from "next/link";
+import { HeaderProps } from "@/types";
 
-export default function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.headerLogo}>
-                <Logo />
-                <span className={styles.headerLogoText}>DoeCerto</span>
-            </div>
-            <nav className={styles.headerNav}></nav>
-        </header>
-    );
+export default function Header({ links = [] }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerLogo}>
+        <Logo />
+        <span className={styles.headerLogoText}>DoeCerto</span>
+      </div>
+      <nav className={styles.headerNav}>
+        {links.length > 0 &&
+          links.map((link, idx) => (
+            <Link
+              key={idx}
+              href={link.href}
+              className={styles.headerNavLink}
+            >
+              {link.label}
+            </Link>
+          ))
+        }
+      </nav>
+    </header>
+  );
 }
