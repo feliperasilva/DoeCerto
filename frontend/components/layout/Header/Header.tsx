9610d6da6b5b630066b2746281f8b4087690481d
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-14 14:16:25 -0300

    feat: implement dynamic header links

--- frontend/components/layout/Header/Header.tsx
+++ frontend/components/layout/Header/Header.tsx
@@ -1,29 +1,27 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./Header.module.css";
 import { Logo } from "@/components";
-import Link from "next/link";
-import { HeaderProps } from "@/types";
+import { getHeaderLinks } from "@/lib";
 
-export default function Header({ links = [] }: HeaderProps) {
-  return (
-    <header className={styles.header}>
-      <div className={styles.headerLogo}>
-        <Logo />
-        <span className={styles.headerLogoText}>DoeCerto</span>
-      </div>
-      <nav className={styles.headerNav}>
-        {links.length > 0 &&
-          links.map((link, idx) => (
-            <Link
-              key={idx}
-              href={link.href}
-              className={styles.headerNavLink}
-            >
-              {link.label}
+export default function Header() {
+    const pathname = usePathname();
+    const links = getHeaderLinks(pathname);
+
+    return (
+        <header className={styles.header}>
+        <div className={styles.headerLogo}>
+            <Logo />
+            <span className={styles.headerLogoText}>DoeCerto</span>
+        </div>
+        <nav className={styles.headerNav}>
+            {links.map((link, idx) => (
+            <Link key={idx} href={link.href} className={styles.headerNavLink}>
+                {link.label}
             </Link>
-          ))
-        }
-      </nav>
-    </header>
-  );
+            ))}
+        </nav>
+        </header>
+    );
 }
