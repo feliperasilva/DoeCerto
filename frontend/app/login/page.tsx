e68de22070eb516255aab73ff10fcae0db663886
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-14 16:56:57 -0300

    feat: add LoginCard component to login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,9 @@
+import { LoginCard } from "@/components";
+
 export default function Page() {
     return (
-        <div>
-            <h1>pagina de login, marcos hot</h1>
-        </div>
+        <>
+            <LoginCard />
+        </>
     );
 }
