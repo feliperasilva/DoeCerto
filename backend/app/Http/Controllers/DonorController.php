8a3955c263da639c1492d1bd789d4d611f30c3c9
feliperasilva <fras2@discente.ifpe.edu.br>
2025-06-17 21:44:54 -0300

    feat: implement DonorController with full CRUD support

---
+++ backend/app/Http/Controllers/DonorController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Donor;
+use Illuminate\Http\Request;
+
+class DonorController extends Controller
+{
+    public function index()
+    {
+        return Donor::all();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'don_name' => 'required|string|max:255',
+            'don_email' => 'required|email|unique:donors,don_email',
+            'don_password' => 'required|string|min:8',
+            'don_description' => 'nullable|string|max:255',
+        ]);
+
+        $data['don_password'] = bcrypt($data['don_password']);
+        return Donor::create($data);
+    }
+
+    public function show($id)
+    {
+        return Donor::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $donor = Donor::findOrFail($id);
+
+        $data = $request->validate([
+            'don_name' => 'sometimes|required|string|max:255',
+            'don_email' => 'sometimes|required|email|unique:donors,don_email,' . $id . ',don_id',
+            'don_password' => 'sometimes|required|string|min:8',
+            'don_description' => 'nullable|string|max:255',
+        ]);
+
+        if (isset($data['don_password'])) {
+            $data['don_password'] = bcrypt($data['don_password']);
+        }
+
+        $donor->update($data);
+        return $donor;
+    }
+
+    public function destroy($id)
+    {
+        Donor::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
