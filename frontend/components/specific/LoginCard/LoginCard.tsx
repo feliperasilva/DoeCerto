63894ac7f8122298cb18280871f5d6b77c937f46
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-21 00:18:58 -0300

    feat: improve login card layout and add signup section

--- frontend/components/specific/LoginCard/LoginCard.tsx
+++ frontend/components/specific/LoginCard/LoginCard.tsx
@@ -1,46 +1,64 @@
 "use client";
 
+// prettier-ignore
+import { Input, Checkbox, Button, InputPassword, ToggleUser } from "@/components";
 import { useState } from "react";
-import { Input, Checkbox, Button, InputPassword } from "@/components";
 import styles from "./LoginCard.module.css";
+import type { UserType } from "@/types";
 
 export default function LoginCard() {
   const [isChecked, setIsChecked] = useState(false);
+  const [selected, setSelected] = useState<UserType>("donor");
 
   return (
     <div className={styles.loginCardContainer}>
-      <div className={styles.loginFormWrapper}>
-        <h2 className={styles.loginTitle}>Entrar Agora</h2>
-        <form className={styles.loginForm}>
-          <Input label="Email" type="email" className={styles.loginInput} />
-          <InputPassword label="Senha" className={styles.loginInput} />
-          <div className={styles.checkboxAndLinkWrapper}>
-            <Checkbox
-              label="Lembrar-me"
-              className={styles.rememberMeCheckbox}
-              onChange={() => setIsChecked(!isChecked)}
-              checked={isChecked}
-            />
-            <a href="#" className={styles.forgotPasswordLink}>
-              Esqueceu sua senha?
-            </a>
-          </div>
-          <Button className={styles.loginButton} size="fullWidth">
-            Entrar
-          </Button>
-        </form>
+      <div className={styles.loginFormContainer}>
+        <div className={styles.loginFormWrapper}>
+          <h1 className={styles.loginTitle}>Entrar Agora</h1>
+          <form className={styles.loginForm}>
+            <Input label="Email" type="email" className={styles.loginInput} />
+            <InputPassword label="Senha" className={styles.loginInput} />
+            <div className={styles.checkboxAndLinkWrapper}>
+              <Checkbox
+                label="Lembrar-me"
+                className={styles.rememberMeCheckbox}
+                onChange={() => setIsChecked(!isChecked)}
+                checked={isChecked}
+              />
+              <a href="#" className={styles.forgotPasswordLink}>
+                Esqueceu sua senha?
+              </a>
+            </div>
+            <Button className={styles.loginButton} size="fullWidth">
+              Entrar
+            </Button>
+          </form>
+        </div>
       </div>
-      <div className={styles.cadastroWrapper}>
-        <p className={styles.cadastroMessage}>
-          Não tem uma conta? <br /> Cadastre-se agora!
-        </p>
-        <Button
-          className={styles.cadastroButton}
-          size="fullWidth"
-          variant="secondary"
-        >
-          Cadastre-se
-        </Button>
+
+      <div className={styles.signupContainer}>
+        <div className={styles.signupWrapper}>
+          <h1 className={styles.signupTitle}>Novo por aqui?</h1>
+          <p className={styles.signupMessage}>
+            Cadastre-se agora mesmo como ONG ou doador e faca a diferenca!
+          </p>
+
+          <ToggleUser selected={selected} setSelected={setSelected} />
+
+          <p className={styles.signupToggleMessage}>
+            {selected === "donor"
+              ? "Como Doador, você pode contribuir para diversas causas e acompanhar o impacto de suas doações."
+              : "Como ONG, você pode cadastrar projetos, receber doações e mostrar o impacto do seu trabalho."}
+          </p>
+
+          <Button
+            className={styles.signupButton}
+            size="fullWidth"
+            variant="secondary"
+          >
+            Cadastre-se
+          </Button>
+        </div>
       </div>
     </div>
   );
