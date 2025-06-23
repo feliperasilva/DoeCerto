1f015950ca22b29b5dfee8cbe95c61e42623ac4f
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-23 17:09:32 -0300

    feat: update login schema to use boolean for rememberMe and remove password length validation

--- frontend/lib/schemas/loginSchema.ts
+++ frontend/lib/schemas/loginSchema.ts
@@ -1,16 +1,10 @@
+// @/lib/schemas/loginSchema.ts
 import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().nonempty("Email é obrigatório").email("Email inválido"),
-  password: z
-    .string()
-    .nonempty("Senha é obrigatória")
-    .min(8, "Senha deve ter no mínimo 8 caracteres"),
-  rememberMe: z
-    .string()
-    .default("Lembre-me")
-    .transform((value) => (value === "true" ? true : false))
-    .optional(),
+  password: z.string().nonempty("Senha é obrigatória"),
+  rememberMe: z.boolean().optional(),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
