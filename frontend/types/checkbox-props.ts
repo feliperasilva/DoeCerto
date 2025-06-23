ddbecfcc6c1e377d355c431c60a1e430329da248
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-23 17:09:00 -0300

    feat: make CheckboxProps optional

--- frontend/types/checkbox-props.ts
+++ frontend/types/checkbox-props.ts
@@ -1,10 +1,8 @@
 export type CheckboxProps = {
-  checked: boolean;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  checked?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   label?: string;
   id?: string;
   name?: string;
   className?: string;
-  // React Hook Form ref
-  ref?: React.Ref<HTMLInputElement>;
 };
