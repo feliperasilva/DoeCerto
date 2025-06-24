d8e44389427234f37c69bf2625ab0177518c78d5
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-06-24 18:36:09 -0300

    feat: add fillable and hidden properties to Ong model

--- backend/app/Models/Ong.php
+++ backend/app/Models/Ong.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ong extends Model
 {
-    //
+    protected $table = 'ongs';
+
+    protected $fillable = [
+        'ong_name',
+        'ong_email',
+        'ong_password',
+        'ong_cnpj',
+        'ong_description',
+    ];
+
+    protected $hidden = [
+        'ong_password',
+    ];
+
+    public $timestamps = true;
 }
