fbae0b174d6053b70133976ae917afa6a6558dc3
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-06-24 17:24:05 -0300

    feat(ong): add Ong model and OngController for API

---
+++ backend/app/Http/Controllers/OngController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ong;
+use Illuminate\Http\Request;
+
+class OngController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ong $ong)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ong $ong)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ong $ong)
+    {
+        //
+    }
+}
