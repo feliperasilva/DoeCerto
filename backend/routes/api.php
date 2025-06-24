18547069ed4653fe4ac2078c9f4529d4e063f73b
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-06-23 22:35:59 -0300

    feat(auth): add auth routes for register, login, and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,5 +4,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\DonorController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('/auth')->group(function () {
+   Route::post('/register', [DonorController::class, 'register']);
+   Route::post('/login', [DonorController::class, 'login']);
+   Route::post('/logout', [DonorController::class, 'logout']); 
+});
+
 Route::apiResource('donors', DonorController::class);
 
