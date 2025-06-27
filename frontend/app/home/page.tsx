2f05fa04b1ac55170f4320199b36f735f3fbe501
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-06-27 00:36:53 -0300

    feat: implement useAuthRedirect hook for authentication check

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,32 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import api from "@/services/api";
-import { useRouter } from "next/navigation";
-
-type Donor = {
-  don_name: string;
-  // outros campos, se quiser
-};
+import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 
 export default function Page() {
-  const [user, setUser] = useState<Donor | null>(null);
-  const router = useRouter();
+  const checkingAuth = useAuthRedirect();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await api.get("/auth/me", { withCredentials: true });
-        setUser(res.data);
-      } catch {
-        router.push("/login");
-      }
-    };
+  if (checkingAuth) return <p>Carregando...</p>;
 
-    fetchUser();
-  }, [router]);
-
-  if (!user) return <p>Carregando...</p>;
-
-  return <div>Bem-vindo, {user.don_name}!</div>;
+  return <div>Bem-vindo!</div>;
 }
