e012755388c1ee021b0761441f024923c3f5789b
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-06-29 12:39:09 -0300

    feat(donation): add StarRating component and favorite button to ONG view

--- frontend/components/specific/Donation/Donation.tsx
+++ frontend/components/specific/Donation/Donation.tsx
@@ -1,28 +1,41 @@
 import React from "react";
 import styles from "./Donation.module.css";
-
+import StarRating from '../../StarRating';
+import { FaHeart, FaMoneyCheckAlt, FaGift } from 'react-icons/fa';
+import { BsPatchCheckFill } from "react-icons/bs";
+import { MdEmail, MdPhone, MdLocationOn } from 'react-icons/md';
+import { IoReturnUpBackOutline } from 'react-icons/io5';
 
 export default function Donation() {
     return(
       <div className={styles.donationContent}>
 
+                {/* lado esquerdo */}
             <div className={styles.donationLeftBox}>
-                <div className={styles.donationOngImage}>
-                   
+
+                {/* cabeçalho lado esquerdo */}
+                <div className={styles.donationHeaderLeft}>
+                  <div className={styles.donationOngImage}>
+                   /* imagem */
+                  </div>
+
+                  <div>
+                    <p className={styles.donationTitleOngLeft}>SOS Gatinho 
+                    <BsPatchCheckFill style={{ marginLeft: 8, color: "#6B39A7" }} />
+                    </p>
+                    <p className={styles.donationParagraphLeft}>Deixe sua Avaliação</p>
+                    <StarRating rating={4.5} />
+                    <p>Muito bom 4.5</p>
+                    <button className={styles.donationButtonFavorite}>
+                      <FaHeart size={20} />Favoritar</button>
+                  </div>
+
                 </div>
+                 {/* fim cabeçalho lado esquerdo */}
 
-                <div className={styles.donationHeader}>
-                    <p className={styles.donationTitleOng}>SOS Gatinho</p>
-                    <p className={styles.donationParagraph}>Deixe sua Avaliação</p>
-                    <p>Muito bom 4.0</p>
-                    <p>Muito bom 4.0</p>
-                 </div>
                  
-            </div>
-
-            <div className={styles.donationRightBox}>
-
-            </div>
+            </div>  
+            {/* fim lado direito */}
       </div>  
     )
 }
