c1ad26b9c071abbba490d03b92d4b7c1e84c601f
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-07-12 20:32:09 -0300

    feat(user_profile): add HTML structure and initial styles for profile page

---
+++ frontend/components/specific/User_Profile/User_Profile.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import styles from "./User_Profile.module.css";
+
+export default function User_Profile() {
+  return (
+    <div className={styles.container}>
+      <h1>User Profile</h1>
+      <p>Welcome to your profile page.</p>
+    </div>
+  );
+}
