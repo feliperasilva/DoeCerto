c4880a1a4e8238be86f4a8d7effc74b3693ad5b7
feliperasilva <fras2@discente.ifpe.edu.br>
2025-07-28 10:15:56 -0300

    feat: Adjust model for Donation

--- backend/app/Models/Donation.php
+++ backend/app/Models/Donation.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    //
+    protected $fillable = [
+        'donor_id',
+        'ong_id',
+        'value',
+        'date',
+        'description',
+    ];
+
+    public function donor()
+    {
+        return $this->belongsTo(Donor::class);
+    }
+
+    public function ong()
+    {
+        return $this->belongsTo(Ong::class);
+    }
 }
+
