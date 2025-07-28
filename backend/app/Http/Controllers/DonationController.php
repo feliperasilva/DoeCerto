6f28dac26d2e24ac352c1b4c928eb494b1e928c8
feliperasilva <fras2@discente.ifpe.edu.br>
2025-07-28 10:31:01 -0300

    feat: Store function for Donation

--- backend/app/Http/Controllers/DonationController.php
+++ backend/app/Http/Controllers/DonationController.php
@@ -9,4 +9,19 @@ class DonationController extends Controller
     public function index () {
         return Donation::with(['donor', 'ong'])->get();
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'donor_id' => 'required|exists:donors,id',
+            'ong_id' => 'required|exists:ongs,id',
+            'value' => 'required|numeric|min:1',
+            'date' => 'required|date|unique:donations,date',
+            'description' => 'nullable|string|max:255',
+        ]);
+
+        $donation = Donation::create($validated);
+
+        return response()->json($donation, 201);
+    }
 }
