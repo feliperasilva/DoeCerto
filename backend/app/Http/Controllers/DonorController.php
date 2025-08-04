828d2e2120b2d72df0abdaf0412243e865cb8226
feliperasilva <fras2@discente.ifpe.edu.br>
2025-08-04 20:06:56 -0300

    feat: adapted store method for donorController

--- backend/app/Http/Controllers/DonorController.php
+++ backend/app/Http/Controllers/DonorController.php
@@ -19,9 +19,17 @@ class DonorController extends Controller
             'don_email' => 'required|email|unique:donors,don_email',
             'don_password' => 'required|string|min:8',
             'don_description' => 'nullable|string|max:255',
+            'don_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
-
+    
+        if ($request->hasFile('don_image')) {
+            $image = $request->file('don_image');
+            $path = $image->store('Donor', 'public'); // salva em public/Donor
+            $data['don_image'] = $path;
+        }
+    
         $data['don_password'] = bcrypt($data['don_password']);
+    
         return Donor::create($data);
     }
 
