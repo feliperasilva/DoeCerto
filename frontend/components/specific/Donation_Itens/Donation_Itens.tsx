4ccff8294c93a3728374e73702f8a546386d2df3
rxl@discente.ifpe.edu.br <rxl@discente.ifpe.edu.br>
2025-08-05 18:37:50 -0300

    feat(html): adicionar estrutura dos modais

--- frontend/components/specific/Donation_Itens/Donation_Itens.tsx
+++ frontend/components/specific/Donation_Itens/Donation_Itens.tsx
@@ -2,14 +2,14 @@
 
 import React, { useState } from 'react';
 import styles from "./Donation_Itens.module.css";
-import { FiChevronDown } from 'react-icons/fi';
+import { FiArrowLeft, FiChevronDown, FiArrowRight } from 'react-icons/fi';
 import { FaPlus, FaMinus } from 'react-icons/fa';
 
 export default function Donation_Itens() {
     const [openModal, setOpenModal] = useState<string | null>(null); 
     
-    const toggleConteudo = () => {
-  setOpenModal((prev) => (prev === 'modal1' ? null : 'modal1'));
+   const toggleConteudo = (modalName: string) => {
+  setOpenModal((prev) => (prev === modalName ? null : modalName));
 };
 
     return(
@@ -18,11 +18,11 @@ export default function Donation_Itens() {
                 <img src="" alt="" />
             </div>
             <div className={styles.donationData}>
-                <button className={styles.buttonFormation} onClick={toggleConteudo}>Dados da Doação
+                <button className={styles.buttonFormation} onClick={() => toggleConteudo('modal1')}>Dados da Doação
                     <FiChevronDown size={24} style={{width: 35, height: 30, alignItems: 'center'}}/></button>
-                <button className={styles.buttonFormation}>Dados da Retirada
+                <button className={styles.buttonFormation} onClick={() => toggleConteudo('modal2')}>Dados da Retirada
                 <FiChevronDown size={24} style={{width: 35, height: 30, alignItems: 'center'}}/></button>
-                <button className={styles.buttonFormation}>Dados do Doador
+                <button className={styles.buttonFormation} onClick={() => toggleConteudo('modal3')}>Dados do Doador
                     <FiChevronDown size={24} style={{width: 35, height: 30, alignItems: 'center'}}/>
                 </button>
             </div>
@@ -59,9 +59,120 @@ export default function Donation_Itens() {
                             <button className={styles.substractAndAdd}><FaPlus style={{color: 'green', width: 15,height: 15}}/></button>
                             <p className={styles.itemName}> Roupas (Sacos ou Caixa)</p>
                         </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.addItens}>
+                            <p style={{fontWeight: 600}}>Não encontrou o item? <a href="#" style={{color: '#6c2bd9', textDecoration: 'none'}}>Clique aqui</a></p>
+                            <button className={styles.buttonNext}>Próximo
+                                <FiArrowRight size={32} color="#6c2bd9" style={{marginLeft: 20}}/>
+                            </button>
+                        </li>
                       </ul>
                     </div>
                   )}
+
+                   {openModal === 'modal2' && (
+                    <div className={styles.modalOverlay}>
+                      <ul className={styles.modalRow}>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Cep:</label>
+                            <div style={{display: 'flex', flexDirection: 'row', gap: 10}}>
+                            <input type="text" className={styles.addressDataInput}/>
+                            <button className={styles.buttonSearch}>Buscar</button>
+                            </div>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Endereço:</label>
+                            
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Número:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Complemento:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Como conheceu a DoeCerto?</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.buttonStart}>
+                            <button className={styles.buttonNext}>
+                                <FiArrowLeft size={32} color="#6c2bd9" style={{marginRight: 20}}/>
+                                Anterior
+                            </button>
+                            <button className={styles.buttonNext}>Próximo
+                                <FiArrowRight size={32} color="#6c2bd9" style={{marginLeft: 20}}/>
+                            </button>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+
+                   {openModal === 'modal3' && (
+                    <div className={styles.modalOverlay}>
+                      <ul className={styles.modalRow}>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Tipo de Pessoa:</label>
+                            <div style={{display: 'flex', flexDirection: 'row', gap: 10}}>
+                                
+                                 <button className={styles.buttonOption}></button>
+                                 <p>Sim</p>
+                                
+                                 <button></button>
+                                 <p>Não</p>
+                                
+                            </div>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>CPF:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Nome:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>E-mail:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>Deseja receber novidades do DoeCerto?</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                         <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>DDD e Telefone 1:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                         <li className={styles.withdrawalData}>
+                            <label htmlFor="" className={styles.nameAddress}>DDD e Telefone 2:</label>
+                            <input type="text" className={styles.addressDataInput}/>
+                        </li>
+                        <hr className={styles.rowSpace}/>
+                        <li className={styles.buttonStart}>
+                            <button className={styles.buttonNext}>
+                                <FiArrowLeft size={32} color="#6c2bd9" style={{marginRight: 20}}/>
+                                Anterior
+                            </button>
+                            <button className={styles.buttonNext}>Próximo
+                                <FiArrowRight size={32} color="#6c2bd9" style={{marginLeft: 20}}/>
+                            </button>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+
 
         </div>
     );
