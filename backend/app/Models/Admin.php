e44bbbcf357ac5d06db1598c5ac119843274368d
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-08-12 20:47:54 -0300

    feat:model admin

---
+++ backend/app/Models/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
